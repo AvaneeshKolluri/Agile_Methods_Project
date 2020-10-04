b4795203f41eefa187eb85308e463d65ea56fc04
ztylergeorge <ztylergeorge@gmail.com>
2020-10-04 17:50:02 -0400

    Changes to test_userStory09 and updates to my Sprint2 tab

--- test_userStory09.py
+++ test_userStory09.py
@@ -24,6 +24,10 @@ class TestUserStory09Class(unittest.TestCase):
         resultsList = userStory09('InputGedFiles/UserStory09_GED/FamilyTree_Test_I3BirthDateChange.ged')
         self.assertEqual(resultsList, ["ERROR: FAMILY: US09: F1: Husband (I1) died 1984-01-09 before child's (I3) birth 2062-07-03",
                                         "ERROR: FAMILY: US09: F1: Wife (I2) died 2017-02-09 before child's (I3) birth 2062-07-03"])
+    
+    def test_userStory09_5(self):
+        resultsList = userStory09('InputGedFiles/UserStory09_GED/FamilyTree_Test_I2DeathDateChange.ged')
+        self.assertEqual(resultsList, ["ERROR: FAMILY: US09: F1: Wife (I2) died 1962-07-02 before child's (I3) birth 1962-07-03"])
 
 if __name__ == "__main__":
     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='./reports'))
