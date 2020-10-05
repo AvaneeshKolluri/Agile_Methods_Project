b7fbaa4a3eb0cbfee3970c0db609fe6a82fc0dd4
Pratim Patel <ppate78@stevens.edu>
2020-10-05 10:37:16 -0400

    fixes

--- pratimTest.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from project2 import Story5
-import project2
-class TestMethods(unittest.TestCase):
-
-    def story05test(self):
-        test1 = ('MarriedbeforeDeath.ged')
-        self.assertTrue(Story5("F1"))
-        
--- test_UserStory6.py
+++ test_UserStory6.py
@@ -4,27 +4,27 @@ from userStory6 import Story6
 class TestUserStory06Class(unittest.TestCase):
 
     def test1_UserStory6(self):
-        resultsList = Story6("death_divorcebefore.ged")
+        resultsList = Story6("InputGedFiles/UserStory06_GED/death_divorcebefore.ged")
         self.maxDiff = None
         self.assertEqual(resultsList, [])
 
     def test2_UserStory6(self):
-        resultsList = Story6("deathbeforeDIV.ged")
+        resultsList = Story6("InputGedFiles/UserStory06_GED/deathbeforeDIV.ged")
         self.maxDiff = None
         self.assertEqual(resultsList, ['Error: divorce occurs after husband death. Divorce Date: 2030-02-02 Husband Death: 2020-01-01'])
 
     def test3_UserStory6(self):
-        resultsList = Story6("deathbeforeDIV_wife.ged")
+        resultsList = Story6("InputGedFiles/UserStory06_GED/deathbeforeDIV_wife.ged")
         self.maxDiff = None
         self.assertEqual(resultsList, ['Error: divorce occurs after wife death. Divorce Date: 2010-02-02 Wife Death: 2009-01-01'])
         
     def test4_UserStory6(self):
-        resultsList = Story6("normal.ged")
+        resultsList = Story6("InputGedFiles/UserStory06_GED/normal.ged")
         self.maxDiff = None
         self.assertEqual(resultsList, [])
 
     def test5_UserStory6(self):
-        resultsList = Story6("husband.ged")
+        resultsList = Story6("InputGedFiles/UserStory06_GED/husband.ged")
         self.maxDiff = None
         self.assertEqual(resultsList, ['Error: divorce occurs after husband death. Divorce Date: 2050-02-02 Husband Death: 2030-01-01'])
 
